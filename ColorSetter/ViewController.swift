1b2c91a30157e7c4033c0b0f63aefcdc1aa72fe4
kirillOff <kirilloffkostya@gmail.com>
2022-06-22 17:38:28 +0300

    add logic and constraints

--- ColorSetter/ViewController.swift
+++ ColorSetter/ViewController.swift
@@ -8,12 +8,56 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var mainView: UIView!
+    
+    @IBOutlet weak var valueRedLabel: UILabel!
+    @IBOutlet weak var valueGreenLabel: UILabel!
+    @IBOutlet weak var valueBlueLabel: UILabel!
+    
+    @IBOutlet weak var redSlider: UISlider!
+    @IBOutlet weak var greenSlider: UISlider!
+    @IBOutlet weak var blueSlider: UISlider!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        redSlider.minimumTrackTintColor = .red
+        greenSlider.minimumTrackTintColor = .green
+        blueSlider.minimumTrackTintColor = .blue
+        
+        redSlider.value = Float.random(in: 0...1)
+        greenSlider.value = Float.random(in: 0...1)
+        blueSlider.value = Float.random(in: 0...1)
+        
+        valueRedLabel.text = String(format: "%.2f", redSlider.value)
+        valueGreenLabel.text = String(format: "%.2f", greenSlider.value)
+        valueBlueLabel.text = String(format: "%.2f", blueSlider.value)
+        
+        getColoFrom(redColor: redSlider.value, greenColor: greenSlider.value, blueColor: blueSlider.value)
     }
-
+    
+    @IBAction func changeColorSettings(_ sender: UISlider) {
+        
+        switch sender.tag {
+        case 1:
+            valueRedLabel.text = String(format: "%.2f", redSlider.value)
+            getColoFrom(redColor: redSlider.value, greenColor: greenSlider.value, blueColor: blueSlider.value)
+        case 2:
+            valueGreenLabel.text = String(format: "%.2f", greenSlider.value)
+            getColoFrom(redColor: redSlider.value, greenColor: greenSlider.value, blueColor: blueSlider.value)
+        case 3:
+            valueBlueLabel.text = String(format: "%.2f", blueSlider.value)
+            getColoFrom(redColor: redSlider.value, greenColor: greenSlider.value, blueColor: blueSlider.value)
+        default:
+            getColoFrom(redColor: redSlider.value, greenColor: greenSlider.value, blueColor: blueSlider.value)
+        }
+        
+    }
+    
+    
+    private func getColoFrom(redColor red: Float, greenColor green: Float, blueColor blue: Float) {
+        mainView.backgroundColor = UIColor(red: CGFloat(red), green: CGFloat(green), blue: CGFloat(blue), alpha: 1)
+    }
 
 }
 
