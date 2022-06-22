9d6c87c29ae67366df2f4f952c3872d981ab94b2
kirillOff <kirilloffkostya@gmail.com>
2022-06-22 14:07:07 +0300

    Initial Commit

---
+++ ColorSetter/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ColorSetter
+//
+//  Created by Konstantin Kirillov on 22.06.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
